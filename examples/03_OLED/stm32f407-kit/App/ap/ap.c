2bbc955793b46e2617abf157f7d91f93b3bf3c1d
hancheol.cho <chcbaram@gmail.com>
2025-04-04 15:29:28 +0900

    OLED 예제 추가

---
+++ examples/03_OLED/stm32f407-kit/App/ap/ap.c
@@ -0,0 +1,39 @@
+#include "ap.h"
+
+
+
+
+
+void apInit(void)
+{
+  cliOpen(_DEF_UART1, 115200);
+  cliLogo();
+
+  for (int i = 0; i < 32; i += 1)
+  {
+    lcdClearBuffer(black);
+    lcdPrintfResize(0, 40 - i, green, 16, "  -- BARAM --");
+    lcdDrawRect(0, 0, LCD_WIDTH, LCD_HEIGHT, white);
+    lcdUpdateDraw();
+    delay(10);
+  }
+  delay(500);
+  lcdClear(black);    
+}
+
+void apMain(void)
+{
+  uint32_t pre_time;
+
+  pre_time = millis();
+  while(1)
+  {
+    if (millis()-pre_time >= 500)
+    {
+      pre_time = millis();
+      ledToggle(_DEF_LED1);
+    }
+
+    cliMain();
+  }
+}
---
+++ examples/03_OLED/stm32f407-kit/App/hw/hw.h
@@ -0,0 +1,19 @@
+#ifndef HW_H_
+#define HW_H_
+
+
+#include "hw_def.h"
+
+
+#include "led.h"
+#include "uart.h"
+#include "cli.h"
+#include "i2c.h"
+#include "lcd.h"
+
+
+bool hwInit(void);
+
+
+
+#endif
