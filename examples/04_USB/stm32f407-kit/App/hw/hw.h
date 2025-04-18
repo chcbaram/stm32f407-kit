9c0cb4e9b4c221e3c582c0fadd045177163a0b99
hancheol.cho <chcbaram@gmail.com>
2025-04-18 16:21:56 +0900

    USB 예제 추가

---
+++ examples/04_USB/stm32f407-kit/App/hw/hw.h
@@ -0,0 +1,20 @@
+#ifndef HW_H_
+#define HW_H_
+
+
+#include "hw_def.h"
+
+
+#include "led.h"
+#include "uart.h"
+#include "cli.h"
+#include "i2c.h"
+#include "lcd.h"
+#include "usbh.h"
+#include "fat_fs.h"
+
+bool hwInit(void);
+
+
+
+#endif
