d11c86a3fe1b0c06c5ce80e06bf273e223528cc7
hancheol.cho <chcbaram@gmail.com>
2025-04-18 17:34:17 +0900

    USB 파일 FLASH 복사 추가

--- examples/05_FLASH/stm32f407-kit/App/hw/src/fat_fs.c
+++ examples/05_FLASH/stm32f407-kit/App/hw/src/fat_fs.c
@@ -2,7 +2,9 @@
 
 #ifdef _USE_HW_FATFS
 #include "fatfs.h"
+#include "flash.h"
 #include "cli.h"
+
 
 static void cliCmd(cli_args_t *args);
 
@@ -75,10 +77,76 @@ void cliCmd(cli_args_t *args)
     ret = true;
   }
 
+  if (args->argc == 2 && args->isStr(0, "flash"))
+  {
+    FIL      fil;
+    FRESULT  fr;
+    UINT     read_bytes;
+    char    *file_name;
+    BYTE     buffer[512];
+    uint32_t flash_addr = 0x08040000;
+
+    file_name = args->getStr(1);
+
+
+    fr = f_open(&fil, file_name, FA_READ);
+    if (fr == FR_OK)
+    {
+      bool flash_ret;
+      uint32_t file_size;
+
+      // for (;;) 
+      // {
+      //   fr = f_read(&fil, buffer, sizeof(buffer), &read_bytes);
+      // }
+
+      file_size = f_size(&fil);
+
+      cliPrintf("file size : %d bytes\n", file_size);
+
+      flash_ret = flashErase(flash_addr, file_size);
+      cliPrintf("flashErase() %s\n", flash_ret ? "OK":"FAIL");
+
+      uint32_t read_index = 0;
+      while(read_index < file_size)
+      {
+        uint32_t read_size;
+
+        read_size = constrain(file_size - read_index, 0, sizeof(buffer));
+        read_bytes = 0;
+        fr = f_read(&fil, buffer, read_size, &read_bytes);
+        if (fr != FR_OK)
+        {
+          cliPrintf("f_read() FAIL\n");
+          break;
+        }
+        
+        flash_ret = flashWrite(flash_addr + read_index, buffer, read_bytes);
+        if (!flash_ret)
+        {
+          cliPrintf("flashWrite() FAIL\n");
+          break;
+        }
+
+        read_index += read_bytes;
+      }
+
+      if (read_index == file_size)
+      {
+        cliPrintf("flashWrite() OK\n");  
+      }
+
+      f_close(&fil);
+    }
+
+    ret = true;
+  }
+
   if (!ret)
   {
     cliPrintf("fatfs info\n");
     cliPrintf("fatfs dir\n");
+    cliPrintf("fatfs flash filename\n");
   }
 }
 
