fc273a7158c6e8869984570e7b7a29b3b684ded7
Hancheol Cho <chcbaram@gmail.com>
2025-04-24 20:49:30 +0900

    BUTTON 추가

---
+++ examples/06_BUTTON/stm32f407-kit/App/hw/hw.c
@@ -0,0 +1,42 @@
+#include "hw.h"
+
+
+
+
+
+bool hwInit(void)
+{
+  cliInit();
+  ledInit();
+  uartInit();
+  for (int i=0; i<UART_MAX_CH; i++)
+  {
+    uartOpen(i, 115200);
+  }
+
+  logPrintf("\r\n[ Firmware Begin... ]\r\n");
+  logPrintf("Booting..Clock\t: %d Mhz\r\n", (int)HAL_RCC_GetSysClockFreq()/1000000);
+  logPrintf("\n");
+  
+  i2cInit();
+  
+  lcdInit();
+  lcdSetFps(20);
+
+  usbhInit();
+  fatfsInit();
+  flashInit();
+  buttonInit();
+  
+  return true;
+}
+
+void delay(uint32_t ms)
+{
+  HAL_Delay(ms);
+}
+
+uint32_t millis(void)
+{
+  return HAL_GetTick();
+}
