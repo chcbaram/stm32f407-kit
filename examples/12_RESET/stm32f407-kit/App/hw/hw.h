40e8cba6650521446b83a0b31e0c2bc10182fa88
Hancheol Cho <chcbaram@gmail.com>
2025-05-25 12:03:48 +0900

    [RESET] 프로젝트 추가

---
+++ examples/12_RESET/stm32f407-kit/App/hw/hw.h
@@ -0,0 +1,22 @@
+#ifndef HW_H_
+#define HW_H_
+
+
+#include "hw_def.h"
+
+
+#include "led.h"
+#include "uart.h"
+#include "cli.h"
+#include "i2c.h"
+#include "lcd.h"
+#include "flash.h"
+#include "button.h"
+#include "rtc.h"
+#include "reset.h"
+
+bool hwInit(void);
+
+
+
+#endif
