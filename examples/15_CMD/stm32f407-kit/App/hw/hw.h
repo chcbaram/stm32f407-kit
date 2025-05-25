9c37126bbc5471d3b60cedaec86f69b14a636abc
Hancheol Cho <chcbaram@gmail.com>
2025-05-25 19:51:59 +0900

    [CMD] 프로젝트 추가

---
+++ examples/15_CMD/stm32f407-kit/App/hw/hw.h
@@ -0,0 +1,27 @@
+#ifndef HW_H_
+#define HW_H_
+
+
+#include "hw_def.h"
+
+
+#include "led.h"
+#include "uart.h"
+#include "cli.h"
+#include "i2c.h"
+#include "lcd.h"
+#include "flash.h"
+#include "button.h"
+#include "rtc.h"
+#include "reset.h"
+#include "etnet.h"
+#include "etnet_udp.h"
+#include "cmd.h"
+#include "qbuffer.h"
+#include "util.h"
+
+bool hwInit(void);
+
+
+
+#endif
