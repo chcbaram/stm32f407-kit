57dbf234eb3cb70ecdc114c2ca29fa26ba908c92
Hancheol Cho <chcbaram@gmail.com>
2025-06-22 13:19:27 +0900

    RTOS_CLI 프로젝트 추가

---
+++ examples/18_RTOS_CLI/stm32f407-kit/App/hw/hw.h
@@ -0,0 +1,21 @@
+#ifndef HW_H_
+#define HW_H_
+
+
+#include "hw_def.h"
+
+
+#include "led.h"
+#include "uart.h"
+#include "cli.h"
+#include "i2c.h"
+#include "lcd.h"
+#include "flash.h"
+#include "button.h"
+
+
+bool hwInit(void);
+
+
+
+#endif
