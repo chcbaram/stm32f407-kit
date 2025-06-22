d32dc2f0f2a19e2e36fdbfecb18af3cc418ce205
Hancheol Cho <chcbaram@gmail.com>
2025-06-22 12:29:36 +0900

    RTOS_START 프로젝트 추가

---
+++ examples/17_RTOS_START/stm32f407-kit/App/hw/hw.c
@@ -0,0 +1,40 @@
+#include "hw.h"
+
+
+
+
+
+bool hwInit(void)
+{
+  cliInit();
+  ledInit();
+  uartInit();
+  for (int i=0; i<UART_MAX_CH; i++)
+  {
+    uartOpen(i, 115200);
+  }
+
+  logPrintf("\r\n[ Firmware Begin... ]\r\n");
+  logPrintf("Booting..Clock\t: %d Mhz\r\n", (int)HAL_RCC_GetSysClockFreq()/1000000);
+  logPrintf("\n");
+  
+  i2cInit();
+  
+  lcdInit();
+  lcdSetFps(20);
+
+  flashInit();
+  buttonInit();
+  
+  return true;
+}
+
+void delay(uint32_t ms)
+{
+  HAL_Delay(ms);
+}
+
+uint32_t millis(void)
+{
+  return HAL_GetTick();
+}
